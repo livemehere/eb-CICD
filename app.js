fc9ebb61f5eaa1e2c33a17a037875633f4efa369
livemehere <rhdxoals@gmail.com>
2022-07-03 16:52:01 +0900

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello from github actions");
+});
+
+app.get("/posts", (req, res) => {
+  const data = [
+    { id: 1, title: "first title", content: "firest content" },
+    { id: 2, title: "second title", content: "second content" },
+    { id: 3, title: "third title", content: "third content" },
+  ];
+  res.json(data);
+});
+
+app.listen(8080, () => {
+  console.log("server is running on port 8080...");
+});
